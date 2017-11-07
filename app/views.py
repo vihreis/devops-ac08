3cefac8c1023b57183c5d0d38a4331f08cfbcad5
Drohiro <rodrigorfsfr3@gmail.com>
2017-11-07 20:57:42 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -63,3 +63,16 @@ def cadastro_cursos(request):
         })
     )
 
+def cadastro_vestibulares(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/cadastro_vestibulares.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Cadastro de vestibulares',
+            'vestibulares': Vestibular.objects.all(),
+            'year':datetime.now().year,
+        })
+)
+
